7e5df4516baa81b7271b42907deaa2f02896a605
Oscar Contreras <ocmchile@gmail.com>
2016-07-05 20:03:43 -0400

    5° commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,14 @@ class PagesController < ApplicationController
 
   def batmanVSsuperman
   end
+
+  def okregistro
+  	if params[:heroe].present? && params[:email].present?
+	  	Voto.create(heroe: params[:heroe], email: params[:email])
+	  	@votos = Voto.all
+	  	redirect_to pages_batmanVSsuperman_path, notice: "El registro fue guardado"
+	else
+		redirect_to pages_batmanVSsuperman_path, notice: "DEBE INGRESAR TODOS LOS DATOS"
+  	end
+  end
 end
